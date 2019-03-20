af58e8a84b140ef35b2c4542edfc13e4caed2c94
Gan Yi Zhong <gan.yizhong@chinasofti.com>
2019-03-20 16:10:49 +0800

    include geokit rails for geocode

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,4 +3,7 @@
 # app/models/vehicle.rb
 class Vehicle < ApplicationRecord
   belongs_to :vehicle_model
+
+  acts_as_mappable lat_column_name: :latitude,
+                   lng_column_name: :longitude
 end
